a05267928c745fe487ba31d0cd9a3364750e6d3c
Ajith krkd <ajith2255iti@gmail.com>
2024-05-14 10:13:01 +0530

    docker vm domine planb-ajithkrkd connected

--- src/services/endpoints/UserEndPoints.js
+++ src/services/endpoints/UserEndPoints.js
@@ -1,11 +1,11 @@
 
 const url = import.meta.env.API_GATEWAY_BASE_URL;
 const userRoutes = {
-    register : 'http://35.244.37.251:8080/user/api/auth/register',
-    login : 'http://35.244.37.251:8080/user/api/auth/login',
-    confirmEmail: `http://35.244.37.251:8080/user/api/auth/confirm-email`, //pass the verification as path variable 
-    get_forgot_password_link: `http://35.244.37.251:8080/user/api/auth/get_forgot_password_link`,
-    forgot_password:`http://35.244.37.251:8080/user/api/auth/forgot_password`,
+    register : 'https://planb-ajithkrkd.online/user/api/auth/register',
+    login : 'https://planb-ajithkrkd.online/user/api/auth/login',
+    confirmEmail: `https://planb-ajithkrkd.online/user/api/auth/confirm-email`, //pass the verification as path variable 
+    get_forgot_password_link: `https://planb-ajithkrkd.online/user/api/auth/get_forgot_password_link`,
+    forgot_password:`https://planb-ajithkrkd.online/user/api/auth/forgot_password`,
 
 //secure only allow by USER
     get_user_details:`/user/api/secure/get_user_details`,
